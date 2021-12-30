7da3760a9a87ba0eec868648fbd2fded03dd4fda
Anton M <antmihlin@gmail.com>
2021-12-30 22:43:44 +0100

    feat(images): cards

--- src/app/components/images-list/images-list.module.ts
+++ src/app/components/images-list/images-list.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { ImagesListComponent } from './images-list/images-list.component';
 import { ImagesListItemComponent } from './images-list-item/images-list-item.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [ImagesListComponent, ImagesListItemComponent],
   exports: [ImagesListComponent, ImagesListItemComponent],
-  imports: [CommonModule, FlexLayoutModule],
+  imports: [CommonModule, FlexLayoutModule, MatCardModule],
 })
 export class ImagesListModule {}
